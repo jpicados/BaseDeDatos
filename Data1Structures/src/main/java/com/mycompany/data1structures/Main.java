568d036260dd49aa79c67218cd5d6363d20816aa
jpicados <98858767+jpicados@users.noreply.github.com>
2022-02-28 20:23:36 -0600

    Semana 6

--- Data1Structures/src/main/java/com/mycompany/data1structures/Main.java
+++ Data1Structures/src/main/java/com/mycompany/data1structures/Main.java
@@ -1,3 +1,5 @@
+package com.mycompany.data1structures;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,13 +13,14 @@
 public class Main {
     
     static int arr[]={12,24,5,7,9};
+    
     public static void main (String [] args){
        
-        /*
+        
         
         System.out.println(Recursion.factorial(4));
         System.out.println(Recursion.buscarElemento(arr, 0, 9, arr.length-1));
-        */ 
+        
         
         
         /*
@@ -33,21 +36,15 @@ public class Main {
         System.out.println(pila.getLenght()+" elementos");
 
         */
+        
         queue cola1=new queue();
         cola1.insertar(46);
-        System.out.println(cola1.getLenght()+" elementos");
         cola1.insertar(12);
-        System.out.println(cola1.getLenght()+" elementos");
         cola1.insertar(87);
-        System.out.println(cola1.getLenght()+" elementos");
         cola1.insertar(125);
-        System.out.println(cola1.getLenght()+" elementos");
         cola1.insertar(30);
-        System.out.println(cola1.getLenght()+" elementos");
         cola1.extraer();
-        System.out.println(cola1.getLenght()+" elementos");
         cola1.isEmpty();
-        System.out.println(cola1.getLenght()+" elementos");
         
         
     }
